6d5492e783509e6b1ac22f806804c5ec9ea11a9c
Sam Burnett <srburnet@gmail.com>
2013-08-01 13:23:57 +0000

    Use new pipeline functions

--- main.go
+++ main.go
@@ -11,28 +11,24 @@ import (
 	"github.com/sburnett/transformer/store"
 )
 
-func getPipelineStages(dbRoot string, workers int) map[string]func() []transformer.PipelineStage {
-	dbPath := func(filename string) string {
-		return filepath.Join(dbRoot, filename)
+func pipelinePrint(dbRoot string, workers int) []transformer.PipelineStage {
+	flagset := flag.NewFlagSet("print", flag.ExitOnError)
+	storePath := flagset.String("leveldb", "", "Print the contents of this LevelDB")
+	keyFormat := flagset.String("key_format", "", "Format keys using this format string")
+	valueFormat := flagset.String("value_format", "", "Format values using this format string")
+	flagset.Parse(flag.Args()[2:])
+	if len(*storePath) == 0 {
+		panic(fmt.Errorf("Invalid leveldb name. Must specify --leveldb."))
 	}
-	pipelineFuncs := make(map[string]func() []transformer.PipelineStage)
-	pipelineFuncs["print"] = func() []transformer.PipelineStage {
-		flagset := flag.NewFlagSet("print", flag.ExitOnError)
-		storePath := flagset.String("leveldb", "", "Print the contents of this LevelDB")
-		keyFormat := flagset.String("key_format", "", "Format keys using this format string")
-		valueFormat := flagset.String("value_format", "", "Format values using this format string")
-		flagset.Parse(flag.Args()[2:])
-		if len(*storePath) == 0 {
-			panic(fmt.Errorf("Invalid leveldb name. Must specify --leveldb."))
-		}
-		store := store.NewLevelDbStore(dbPath(*storePath))
-		return diagnostics.RecordPrinterPipeline(store, *keyFormat, *valueFormat)
-	}
-	return pipelineFuncs
+	store := store.NewLevelDbStore(filepath.Join(dbRoot, *storePath))
+	return diagnostics.RecordPrinterPipeline(store, *keyFormat, *valueFormat)
 }
 
 func main() {
-	name, pipeline := transformer.ParsePipelineChoice(getPipelineStages)
+	pipelineFuncs := map[string]transformer.PipelineFunc{
+		"print": pipelinePrint,
+	}
+	name, pipeline := transformer.ParsePipelineChoice(pipelineFuncs)
 
 	go cube.Run(fmt.Sprintf("transformer_diagnostics_%s", name))
 
